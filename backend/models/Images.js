5b561f6002af6558e05658965be719d12de485dd
Mohd Zain <zainmohd1998@gmail.com>
2025-03-30 13:02:28 +0530

    add Images SQL model

---
+++ backend/models/Images.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../mysql");
+
+const Image = sequelize.define(
+  "Image",
+  {
+    image_id: {
+      type: DataTypes.STRING(6),
+      allowNull: false,
+      unique: true,
+    },
+    userId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+      defaultValue: "default",
+    },
+    file_key: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    pass: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    passval: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    expires_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    isPermanent: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+  },
+  {
+    tableName: "images",
+    timestamps: true, // Auto adds createdAt/updatedAt
+  }
+);
+
+module.exports = Image;
